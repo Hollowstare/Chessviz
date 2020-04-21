00419172c983c6f5d061401f2ac41814a99f61fe
Hollowstare <Magickpen@yandex.ru>
2020-04-21 18:20:35 +0800

    Merge branch 'changes'

--- src/drawBoard.cpp
+++ src/drawBoard.cpp
@@ -1,25 +1,17 @@
 #include <iostream>
-#include <Windows.h>
 using namespace std;
 
-void Chessplace(char pole[][9]) {
-	cout << endl;
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			if (i == 8 || (j == 0)) {
-				HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-				SetConsoleTextAttribute(handle, 9);
-			}
-			if (j == 0) {
-				cout << " ";
-			}
-			cout << pole[i][j] << " ";
-			HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-			SetConsoleTextAttribute(handle, 7);
-		}
-		cout << endl;
-	}
-	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(handle, 7);
-	cout << endl;
+void Chessplace(char pole[][9])
+{
+    cout << endl;
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (j == 0) {
+                cout << " ";
+            }
+            cout << pole[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,27 @@
 #include "header.h"
-#include <Math.h>
 #include <cstring>
 #include <iostream>
-#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-	char coordinates[6];
-	char pole[9][9] = { {'1', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-					   {'2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-					   {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-					   {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-					   {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-					   {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-					   {'7', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-					   {'8', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-					   {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'} };
-	Chessplace(pole);
-	cout << "White pieces go first (lower case)" << endl << endl;
-	while (true) {
-		cin >> coordinates;
-		CheckTypePiece(coordinates, pole);
-		Chessplace(pole);
-	}
-	return 0;
+    char coordinates[6];
+    char pole[9][9] = {{'1', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                       {'2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                       {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {'7', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                       {'8', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+                       {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
+    Chessplace(pole);
+    cout << "White pieces go first (lower case)" << endl << endl;
+    while (true) {
+        cin >> coordinates;
+        CheckTypePiece(coordinates, pole);
+        Chessplace(pole);
+    }
+    return 0;
 }
