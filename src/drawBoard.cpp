e6f042678300076be7c0ce324546a4134ab1473a
Hollowstare <Magickpen@yandex.ru>
2020-04-21 16:11:40 +0800

    Fix drawBoard.cpp

--- src/drawBoard.cpp
+++ src/drawBoard.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-
+#include <Windows.h>
 using namespace std;
 
-void Chessplace(char pole[][9])
-{
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            cout << pole[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
+void Chessplace(char pole[][9]) {
+	cout << endl;
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (i == 8 || (j == 0)) {
+				HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+				SetConsoleTextAttribute(handle, 9);
+			}
+			if (j == 0) {
+				cout << " ";
+			}
+			cout << pole[i][j] << " ";
+			HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(handle, 7);
+		}
+		cout << endl;
+	}
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(handle, 7);
+	cout << endl;
 }
