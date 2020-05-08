2f814c0ee8a1fc1ac189c0518410ba78120617de
Hollowstare <Magickpen@yandex.ru>
2020-05-08 17:21:18 +0800

    Change header.h

--- src/header.h
+++ src/header.h
@@ -1,6 +1,6 @@
 #ifndef ADD_H
 #define ADD_H
-void CheckTypePiece(char turn[], char pole[][9]);
+bool CheckTypePiece(char turn[], char pole[][9]);
 void Chessplace(char pole[][9]);
 void movePawn(char coordinates[], char pole[][9]);
 bool checkMove(char coordinates[], char pole[][9]);
@@ -8,5 +8,5 @@ void moveOtherFigures(char coordinates[], char pole[][9]);
 bool CheckBoard(int a, int b, int c, int d);
 bool CheckMove(char coordinates[], char pole[][9]);
 bool CheckType(int beginY, int beginX, char pole[][9], char coordinates[]);
-bool CheckMove(char coordinates[], char pole[][9], int endY, int endX);
+bool CheckMove(char coordinates[], char pole[][9], int endY, int endX, int beginY, int beginX);
 #endif
