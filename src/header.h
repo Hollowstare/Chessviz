20e5edcb5ba2f439dd5d07f7c765c21ffd067892
Hollowstare <Magickpen@yandex.ru>
2020-05-08 17:25:46 +0800

    Fix formatting header.h

--- src/header.h
+++ src/header.h
@@ -8,5 +8,11 @@ void moveOtherFigures(char coordinates[], char pole[][9]);
 bool CheckBoard(int a, int b, int c, int d);
 bool CheckMove(char coordinates[], char pole[][9]);
 bool CheckType(int beginY, int beginX, char pole[][9], char coordinates[]);
-bool CheckMove(char coordinates[], char pole[][9], int endY, int endX, int beginY, int beginX);
+bool CheckMove(
+        char coordinates[],
+        char pole[][9],
+        int endY,
+        int endX,
+        int beginY,
+        int beginX);
 #endif
