00bb8d060391c12782c57743cc9e7ebfafca144c
Hollowstare <Magickpen@yandex.ru>
2020-05-08 17:21:10 +0800

    Add new tests

--- test/main.cpp
+++ test/main.cpp
@@ -17,41 +17,61 @@ char pole[9][9] = {{'1', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
 TEST_CASE("Incorrect quiet move", "Incorrect")
 {
     int endY = 4, endX = 5;
+    int beginY = 2, beginX = 5;
     pole[3][5] = 'P';
     char coordinates[6] = "e2-e4";
-    REQUIRE(CheckMove(coordinates, pole, endY, endX) == false);
+    REQUIRE(CheckMove(coordinates, pole, endY, endX, beginY, beginX) == false);
 }
 
 TEST_CASE("Сorrect quiet move", "correct")
 {
     int endY = 4, endX = 5;
+    int beginY = 2, beginX = 5;
     pole[3][5] = ' ';
+    pole[1][5] = 'p';
     char coordinates[6] = "e2-e4";
-    REQUIRE(CheckMove(coordinates, pole, endY, endX) == true);
+    REQUIRE(CheckMove(coordinates, pole, endY, endX, beginY, beginX) == true);
 }
 
 TEST_CASE("correct attack move", "Correct")
 {
     int endY = 4, endX = 5;
+    int beginY = 2, beginX = 5;
     pole[3][5] = 'P';
     char coordinates[6] = "e2xe4";
-    REQUIRE(CheckMove(coordinates, pole, endY, endX) == true);
+    REQUIRE(CheckMove(coordinates, pole, endY, endX, beginY, beginX) == true);
 }
 
 TEST_CASE("Incorrect attack move", "Incorrect")
 {
     int endY = 4, endX = 5;
+    int beginY = 2, beginX = 5;
     pole[3][5] = ' ';
     char coordinates[6] = "e2xe4";
-    REQUIRE(CheckMove(coordinates, pole, endY, endX) == false);
+    REQUIRE(CheckMove(coordinates, pole, endY, endX, beginY, beginX) == false);
+}
+
+TEST_CASE("Incorrect move. Empty start chessboard square", "Incorrect")
+{
+    int endY = 4, endX = 5;
+    int beginY = 6, beginX = 5;
+    pole[3][5] = 'p';
+    pole[5][5] = ' ';
+    char coordinates[6] = "e6xe4";
+    REQUIRE(CheckMove(coordinates, pole, endY, endX, beginY, beginX) == false);
 }
 
 TEST_CASE("The move isn't off the board", "correct")
 {
-    REQUIRE(CheckBoard(2, 4, 5, 9) == false);
-    REQUIRE(CheckBoard(1, 4, 5, 7) == true);
-    REQUIRE(CheckBoard(4, 3, 6, 8) == true);
-    REQUIRE(CheckBoard(9, 4, 2, 2) == false);
+    //(CheckBoard(beginX, beginY, endX, endY);
+    // e2-e4
+    REQUIRE(CheckBoard(5, 2, 5, 4) == true);
+    // e2-e9
+    REQUIRE(CheckBoard(5, 2, 5, 9) == false);
+    // e0-e5
+    REQUIRE(CheckBoard(5, 0, 5, 5) == false);
+    // a2-a5
+    REQUIRE(CheckBoard(1, 2, 1, 5) == true);
 }
 
 TEST_CASE("correct check type 'Q'", "correct")
@@ -132,4 +152,22 @@ TEST_CASE("correct Check type 'b'", "correct")
     pole[7][3] = 'b';
     char coordinates[7] = "bb8-a3";
     REQUIRE(CheckType(beginY, beginX, pole, coordinates) == true);
+}
+
+TEST_CASE("Incorrect input", "correct")
+{
+    char coordinates[8] = "eds-sde";
+    REQUIRE(CheckTypePiece(coordinates, pole) == false);
+}
+
+TEST_CASE("Incorrect input2", "correct")
+{
+    char coordinates[8] = "e2e4";
+    REQUIRE(CheckTypePiece(coordinates, pole) == false);
+}
+
+TEST_CASE("Correct input", "correct")
+{
+    char coordinates[8] = "e2-e4";
+    REQUIRE(CheckTypePiece(coordinates, pole) == true);
 }
